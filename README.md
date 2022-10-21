99598d4ec7b7fce8fdeb94372067594e1419291d
Deo Valiandro. M <valiandrod@gmail.com>
2022-10-21 13:23:28 +0800

    init
    Signed-off-by: Deo Valiandro. M <valiandrod@gmail.com>

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sistem Informasi Billboard
+
+Untuk jalankan:
+
+1. Download,
+2. Simpan di XAMPP,
+3. Install composer,
+4. Run `composer install`, ikuti sampai selesai,
+5. Run `php artisen serve`,
+6. Buka Firefox, lalu ke `localhost:8000`
+
+__Deo Valiandro. M__
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
+  <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
+  <title>Laravel</title>
+  <style>
+    body {
+      font-family: 'SF Font', sans-serif !important;
+    }
+
+    #map {
+      height: 80vh;
+    }
+  </style>
+</head>
+
+<body>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+      </a>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          Home
+        </a>
+
+        <a class="navbar-item">
+          Documentation
+        </a>
+
+        <div class="navbar-item has-dropdown is-hoverable">
+          <a class="navbar-link">
+            More
+          </a>
+
+          <div class="navbar-dropdown">
+            <a class="navbar-item">
+              About
+            </a>
+            <a class="navbar-item">
+              Jobs
+            </a>
+            <a class="navbar-item">
+              Contact
+            </a>
+            <hr class="navbar-divider">
+            <a class="navbar-item">
+              Report an issue
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Sign up</strong>
+            </a>
+            <a class="button is-light">
+              Log in
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <!-- jangan dihapus -->
+  <!-- @if (Route::has('login'))
+    <div class="">
+      @auth
+      <a href="{{ url('/home') }}">Home</a>
+      @else
+      <a href="{{ route('login') }}">Log in</a>
+
+      @if (Route::has('register'))
+      <a href="{{ route('register') }}">Register</a>
+      @endif
+      @endauth
+    </div>
+    @endif -->
+  <section class="section pt-0">
+    <div class="container">
+      <h1 class="title has-text-centered">
+        Sistem Informasi Billboard
+      </h1>
+      <div class="card">
+        <div class="card-image">
+          <div id="map"></div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer class="footer p-4">
+    <div class="content has-text-centered">
+      <p>
+      By <a href="https://deo.or.id">Deo Valiandro. M</a>. The source code is licensed
+      <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+      is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      </p>
+    </div>
+  </footer>
+
+  <script>
+    var map = L.map('map').setView([-5.131555, 119.487473], 14);
+    var layerll = L.tileLayer('http://{s}.google.com/vt?lyrs=s&x={x}&y={y}&z={z}', {
+      maxZoom: 20,
+      subdomains: ['mt0', 'mt1', 'mt2', 'mt3']
+    }).addTo(map);
+
+    function onMapClick(e) {
+      popup
+        .setLatLng(e.latlng)
+        .setContent('You clicked the map at ' + e.latlng.toString())
+        .openOn(map);
+    }
+
+    map.on('click', onMapClick);
+
+    document.getElementsByClassName('leaflet-control-attribution')[0].innerHTML = "© Deo | 🌱 dengan ❤️ dari 🇮🇩 dengan bantuan <a href=\"https://leafletjs.com\" title=\"A JavaScript library for interactive maps\">Leaflet</a>";
+  </script>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+</body>
+
+</html>
