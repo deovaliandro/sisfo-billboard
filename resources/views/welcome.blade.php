4c4abe3fbe0d24ffa9d39cebeb8c515e28dfd88c
Deo Valiandro. M <valiandrod@gmail.com>
2022-10-21 13:24:43 +0800

    fix: judul
    Signed-off-by: Deo Valiandro. M <valiandrod@gmail.com>

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.2/dist/leaflet.css" integrity="sha256-sA+zWATbFveLLNqWO2gtiw3HL/lh1giY/Inf1BJ0z14=" crossorigin="" />
   <script src="https://unpkg.com/leaflet@1.9.2/dist/leaflet.js" integrity="sha256-o9N1jGDZrf5tS+Ft4gbIK7mYMipq9lqpVJ91xHSyKhg=" crossorigin=""></script>
-  <title>Laravel</title>
+  <title>Sistem Informasi Billboard</title>
   <style>
     body {
       font-family: 'SF Font', sans-serif !important;
